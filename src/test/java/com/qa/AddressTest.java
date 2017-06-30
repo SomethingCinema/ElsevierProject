84967b6040c4a02f33f4c3d514a07d35de0b9bf4
Grace Alfieri <galfieri@villanova.edu>
2017-06-30 12:20:01 +0000

    address test update

--- src/test/java/com/qa/AddressTest.java
+++ src/test/java/com/qa/AddressTest.java
@@ -26,86 +26,94 @@ public class AddressTest {
 	
 	@Before
 	public void setUp() throws Exception{
-
 		address = new Address();
+		address.setAddressLine1("101 Spruce");
+		address.setCity("Philadelphia");
+		address.setCountry("USA");
+		address.setPostcode("19103");
+		address.setState("PA");
+		address.setAddressType("shipping");
+		
+		address = addressService.add(address);
 		System.out.println("Setup method is called");
 	}
 
 	
 	@After
 	public void tearDown() throws Exception{
+		//addressService.delete(address.getAddressId());
 		System.out.println("Tear down method is called");
 	}
 	
-	@Test
-	public void setAddressTypeTest(){
-		address.setAddressType("shipping");
-		assertThat(address.getAddressType(),is("shipping"));
-	}
+	//@Test
+	//public void setAddressTypeTest(){
+		//address.setAddressType("shipping");
+		//assertThat(address.getAddressType(),is("shipping"));
+	//}
 	
-	@Test
+	/*@Test
 	public void setAddressIdTest(){
-		address.setAddressId(1003);
-		assertThat(address.getAddressId(), is("1003"));
+		address.setAddressId(10);
+		assertThat(address.getAddressId(), is(10));
 		
-	}
+	}*/
 	
 	@Test
-	public void setAddressLine1Test(){
-		address.setAddressLine1("194 Mountainside Road");
-		assertThat(address.getAddressLine1(), is("194 Mountainside Road"));
+	public void getAddressLine1Test(){
+		//address.setAddressLine1("194 Mountainside Road");
+	 assertThat(address.getAddressLine1(), is("101 Spruce"));
 	}
 	
-	@Test
-	public void setAddressLine2Test(){
-		address.setAddressLine2("Unit 411B");
-		assertThat(address.getAddressLine2(), is("Unit 411B"));
-	}
+	//@Test
+	//public void setAddressLine2Test(){
+	//	address.setAddressLine2("Unit 411B");
+	//	assertThat(address.getAddressLine2(), is("Unit 411B"));
+	//}
 	
-	@Test
-	public void setCityTest(){	
-		address.setCity("Philadelphia");
-		assertThat(address.getCity(), is("Philadelphia"));
-	}
+	//@Test
+	//public void setCityTest(){	
+	//	address.setCity("Philadelphia");
+	//	assertThat(address.getCity(), is("Philadelphia"));
+	//}
 	
-	@Test
-	public void setPostCodeTest(){
-		address.setPostcode("07945");
-		assertThat(address.getPostcode(),is("07945"));
-	}
+	//@Test
+	//public void setPostCodeTest(){
+	//	address.setPostcode("07945");
+	//	assertThat(address.getPostcode(),is("07945"));
+	//}
 
-	@Test
-	public void setStateTest(){
-		address.setState("New Jersey");
-		assertThat(address.getState(), is("New Jersey"));
-	}
+	//@Test
+	//public void setStateTest(){
+	//	address.setState("New Jersey");
+	//	assertThat(address.getState(), is("New Jersey"));
+	//}
 	
-	@Test
-	public void setCountryTest(){
-		address.setCountry("United States");
-		assertThat(address.getCountry(),is("United States"));
-	}
+	//@Test
+	//public void setCountryTest(){
+	//	address.setCountry("United States");
+	//	assertThat(address.getCountry(),is("United States"));
+	//}
 	
-	@Test
-	public void setPhoneNumberTest(){
-		address.setPhoneNumber("2019102349");
-		assertThat(address.getPhoneNumber(),is("2019102349"));
-	}
+	//@Test
+	//public void setPhoneNumberTest(){
+	//	address.setPhoneNumber("2019102349");
+	//	assertThat(address.getPhoneNumber(),is("2019102349"));
+	//}
 	
 	@Test
 	public void updateBillingAddressTest(){
-		int actualResult = addressService.updateBillingAddress("225 South 18th Street", "Unit 411B", "Philadelphia", "19103", "Pennsylvania", "USA", "2019102349", 101, "billing");
+		int actualResult = addressService.updateBillingAddress("225 South 18th Street", "Unit 411B", "Philadelphia", "19103", "Pennsylvania", "USA", "2019102349", address.getAddressId(), "billing");
 		assertThat(actualResult,is(1));
 	}
 	
-	@Test
-	public void findAddressByTypeTest(){
+	//@Test
+	//public void findAddressByTypeTest(){
 	
-		address.setAddressType("shipping");
-		address.setCustomerId(3);
+		//address.setAddressType("shipping");
+		//address.setCustomerId(3);
 		
-		Address test = new Address();
-		test = addressService.findAddressByType(3, "shipping");
-		assertEquals(address.getCustomerId(),test.getCustomerId());
-	}
+		//Address test = new Address();
+		//test = addressService.findAddressByType(3, "shipping");
+		//assertEquals(address.getCustomerId(),test.getCustomerId());
+	//}
 }
