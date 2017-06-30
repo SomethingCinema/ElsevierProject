b47caebad8fd6deab74a7702739b2099529b286f
rebeccaplease <rebeccaplease@gmail.com>
2017-06-30 00:15:33 -0400

    cleanup service classes

--- src/main/java/com/qa/services/CustomerService.java
+++ src/main/java/com/qa/services/CustomerService.java
@@ -10,73 +10,39 @@ import com.qa.repositories.CustomerRepository;
 
 @Service
 public class CustomerService {
-	
+
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-	public void delete(int id){
+
+	public void delete(int id) {
 		customerRepository.delete(id);
 	}
-	
-	public Customer add(Customer c){
+
+	public Customer add(Customer c) {
 		Iterable<Customer> allCustomers = customerRepository.findAll();
-		
-		for(Customer current: allCustomers){
-			if(current.getEmail().equalsIgnoreCase(c.getEmail())){
-				return null; //email already exists
+
+		for (Customer current : allCustomers) {
+			if (current.getEmail().equalsIgnoreCase(c.getEmail())) {
+				return null; // email already exists
 			}
 		}
 		return customerRepository.save(c);
 
 	}
-	
-	public Customer loginProcess(String email,String password) {
-//		Iterable<Customer> allCustomers = customerRepository.findAll();
-//		
-//		for(Customer c: allCustomers){
-//			if(c.getEmail().equalsIgnoreCase(email) && c.getPassword().equals(password)){
-//				return c;
-//			}
-//		}
-//		return null;
+
+	public Customer loginProcess(String email, String password) {
 		return customerRepository.loginProcess(email, password);
 	}
-	
-	public int updateCustomer(String firstName,
-			String lastName,
-			String email,
-			 int customerId){
-		
-//		Customer c = customerRepository.findOne(customerId);
-//		
-//		c.setFirstName(firstName);
-//		c.setLastName(lastName);
-//		c.setEmail(email);
-//		
-//		if(customerRepository.save(c) != null){ 
-//			return 1;
-//		}
-//		else{
-//			return 0;
-//		}
+
+	public int updateCustomer(String firstName, String lastName, String email, int customerId) {
 		return customerRepository.updateCustomer(firstName, lastName, email, customerId);
 	}
-	
-	public int updatePassword(String password, int customerId){
-		
-//		Customer c = customerRepository.findOne(customerId);
-//		
-//		c.setPassword(password);
-//		
-//		if(customerRepository.save(c) != null){ 
-//			return 1;
-//		}else{
-//			return 0;
-//		}
+
+	public int updatePassword(String password, int customerId) {
 		return customerRepository.updatePassword(password, customerId);
 	}
-	
-	public Customer findOne(int customerId){
+
+	public Customer findOne(int customerId) {
 		return customerRepository.findOne(customerId);
 	}
 
