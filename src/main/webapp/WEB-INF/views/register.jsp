d186405f09668a328465d1e7551c8643cc568de5
rebeccaplease <rebeccaplease@gmail.com>
2017-06-29 23:24:02 -0400

    registration empty tests

--- src/main/webapp/WEB-INF/views/register.jsp
+++ src/main/webapp/WEB-INF/views/register.jsp
@@ -87,7 +87,6 @@
 		</div>
 
 	</div>
-	</div>
 
 
 	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
--- src/test/java/com/qa/ValidationTest.java
+++ src/test/java/com/qa/ValidationTest.java
@@ -3,17 +3,25 @@ package com.qa;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.qa.controllers.Validate;
+import com.qa.models.Customer;
 
 public class ValidationTest {
 	
-	//private Customer customer;
+	private Customer c;
 
-//	@Before
-//	public void setUp() throws Exception {
-//	}
+	@Before
+	public void setUp() throws Exception {
+		c = new Customer();
+		c.setCustomerId(20);
+		c.setEmail("hello@gmail.com");
+		c.setFirstName("First");
+		c.setLastName("Last");
+		c.setPassword("password");
+	}
 //
 //	@After
 //	public void tearDown() throws Exception {
@@ -22,9 +30,60 @@ public class ValidationTest {
 	@Test
 	public void validateLoginEmailEmptyTest() {
 		String email = "";
-		String password = "";
+		String password = "password";
 		String result = Validate.validateLogin(email, password);
 		assertThat(result, is("Please enter an email"));
 	}
+	@Test
+	public void validateLoginPasswordEmptyTest() {
+		String email = "hello@gmail.com";
+		String password = "";
+		String result = Validate.validateLogin(email, password);
+		assertThat(result, is("Please enter a password"));
+	}
+	@Test
+	public void validateLoginEmailFailTest() {
+		String email = "emailhere";
+		String password = "password";
+		String result = Validate.validateLogin(email, password);
+		assertThat(result, is("Please enter a valid email"));
+	}
+	@Test
+	public void validateLoginEmailSuccessTest() {
+		String email = "hello@gmail.com";
+		String password = "password";
+		String result = Validate.validateLogin(email, password);
+		assertThat(result, is(""));
+	}
+	
+	@Test
+	public void validateRegistrationFirstNameEmpty(){
+		c.setFirstName("");
+		String agreement = "agree";
+		String result = Validate.validateRegistration(c, agreement);
+		assertThat(result, is("Registration failed - please enter a first name"));
+	}
+	@Test
+	public void validateRegistrationLastNameEmpty(){
+		c.setLastName("");
+		String agreement = "agree";
+		String result = Validate.validateRegistration(c, agreement);
+		assertThat(result, is("Registration failed - please enter a last name"));
+	}
+	@Test
+	public void validateRegistrationEmailEmpty(){
+		c.setEmail("");
+		String agreement = "agree";
+		String result = Validate.validateRegistration(c, agreement);
+		assertThat(result, is("Registration failed - please enter an email"));
+	}
+	@Test
+	public void validateRegistrationPasswordEmpty(){
+		c.setPassword("");
+		String agreement = "agree";
+		String result = Validate.validateRegistration(c, agreement);
+		assertThat(result, is("Registration failed - please enter a password"));
+	}
+
 
 }
