bf658844ce99bddd3197a3c34bee349c65cd8db3
rebeccaplease <rebeccaplease@gmail.com>
2017-06-29 23:09:25 -0400

    return first error message in Validate instead of last

--- src/test/java/com/qa/ValidationTest.java
+++ src/test/java/com/qa/ValidationTest.java
@@ -1,26 +1,30 @@
 package com.qa;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+
+import com.qa.controllers.Validate;
 
 public class ValidationTest {
 	
 	//private Customer customer;
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+//	@Before
+//	public void setUp() throws Exception {
+//	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//	}
 
 	@Test
-	public void validateLoginTest() {
-		fail("Not yet implemented");
+	public void validateLoginEmailEmptyTest() {
+		String email = "";
+		String password = "";
+		String result = Validate.validateLogin(email, password);
+		assertThat(result, is("Please enter an email"));
 	}
 
 }
